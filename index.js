0584aa5caeb683fdf17b970f2134b8ead248beb6
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-16 17:10:09 +0800

    styling grid when hovered
    added eventlisterners to grid items so it changes
    color when mouse enters

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const container = document.querySelector('#container');
 
+createGrid();
 function createGrid() {
   for(let i = 0; i < 16*16; i++) {
     const grid = document.createElement('div');
+    grid.classList.add('grid-item');
+    grid.textContent = 'grid';
     container.appendChild(grid);
   }
 }
+
+let grid = document.querySelector('grid-item');
+
+grid.addEventListener('mouseenter', () => {
+  grid.style.backgroundColor = 'pink';
+})
