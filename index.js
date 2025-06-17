cefb8a76bc93f36e72d281c7ddb1b048be887a9e
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-06-17 13:43:43 +0800

    grid size button
    added an argument into create grid element so
    grid size can be changed based on the input in
    the prompt when clicking the grid size button.
    created a function to delete existing grid so grid
    can be replaced with a new size each button click.

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const container = document.querySelector('#container');
-const gridSize = document.createElement('button');
+const gridSizeBtn = document.createElement('button');
+const body = document.querySelector('body');
+body.insertBefore(gridSizeBtn, container);
+gridSizeBtn.textContent = 'select grid size'
 
-createGrid();
-function createGrid() {
-  for(let i = 0; i < 16*16; i++) {
+const defaultSize = 16;
+
+let gridSzie = defaultSize;
+
+createGrid(defaultSize);
+function createGrid(gridSize) {
+  for(let i = 0; i < gridSize*gridSize; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid-item');
-    grid.textContent = 'grid';
     container.appendChild(grid);
+    grid.style.width = `${100/gridSize}%`
+    grid.style.height = `${100/gridSize}%`
     grid.addEventListener('mouseenter', () => {
       grid.style.backgroundColor = 'pink';
     })
   }
 }
+
+function deleteGrid() {
+  container.innerHTML = '';
+}
+
+gridSizeBtn.addEventListener('click', () => {
+  const gridSize = prompt('select the number of squares per side', '')
+  deleteGrid();
+  createGrid(gridSize)
+})
